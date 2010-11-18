82aa97b12bff7403fa96e435cdde6d87d52936d4
chilltassapola <chilltassapola@7427e34b-43ad-5727-de77-e22b264e6e80>
2010-11-18 07:44:30 +0000

    add myfilewriter

--- trunk/java/src/proxy/FrontEnd.java
+++ trunk/java/src/proxy/FrontEnd.java
@@ -5,14 +5,23 @@ import util.MyUtil;
 public class FrontEnd {
 
 	private String url;
+	private DownloadResult dlResult;
 	
 	public FrontEnd(String url) {
 		this.url = url;
 	}
 
 	public void process() {
-		DownloadResult dlResult = Downloader.download(url);
-		MyUtil.print(MyUtil.getDataAfterModifyUrl(dlResult.getData(), MyUtil.defaultRewriteUrlHost, url));
+		dlResult = Downloader.download(url);
+		byte[] newData = MyUtil.getDataAfterModifyUrl(dlResult.getData(), url);
+		dlResult.setData(newData);
+		//MyUtil.print(MyUtil.getDataAfterModifyUrl(dlResult.getData(), MyUtil.defaultRewriteUrlHost, url));
 		///MyUtil.print(dlResult.getData());
+	}
+	
+	public void writeFile(String location) {
+		MyFileWriter myFileWriter = new MyFileWriter(location);
+		myFileWriter.write(dlResult.getData());
+		myFileWriter.close();
 	}
 }
---
+++ trunk/java/src/proxy/MyFileWriter.java
@@ -0,0 +1,34 @@
+package proxy;
+
+import java.io.*;
+
+public class MyFileWriter {
+
+	private FileOutputStream fos;
+	
+	public MyFileWriter(String location) {
+		try {
+			fos = new FileOutputStream(new File(location));
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void write(byte[] data) {
+		for (int i=0; i < data.length; i++) {
+			try {
+				fos.write(data[i]);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	public void close() {
+		try {
+			fos.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
