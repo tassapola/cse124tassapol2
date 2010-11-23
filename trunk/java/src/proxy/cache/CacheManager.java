df5e8d2cacb2349d2fa039ed5a75635e463bdcc3
chilltassapola <chilltassapola@7427e34b-43ad-5727-de77-e22b264e6e80>
2010-11-23 11:16:51 +0000

    add refresh data

--- trunk/java/src/proxy/cache/CacheManager.java
+++ trunk/java/src/proxy/cache/CacheManager.java
@@ -19,7 +19,7 @@ public class CacheManager {
 	public byte[] getData(String url, Downloader downloader) {
 		CacheData o = cache.get(url);
 		if (o == null) {
-			DownloadResult dr = downloader.download(url);
+			DownloadResult dr = Downloader.download(url);
 			CacheData c = new CacheData(dr.getDate(), dr.getData());
 			cache.put(url, c);
 			return dr.getData();
@@ -27,7 +27,7 @@ public class CacheManager {
 			//check if-modified since
 			if (Downloader.isModifiedSince(url, o.getDate())) {
 				//need to refresh cache
-				DownloadResult dr = downloader.download(url);
+				DownloadResult dr = Downloader.download(url);
 				CacheData c = new CacheData(dr.getDate(), dr.getData());
 				cache.put(url, c);
 				return dr.getData();
