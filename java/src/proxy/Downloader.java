4ec57faf0658f511f68214370f32a9ba6ee3f48a
chilltassapola <chilltassapola@7427e34b-43ad-5727-de77-e22b264e6e80>
2010-11-23 11:11:13 +0000

    add cache and ifmodifiedsince

--- java/src/proxy/Downloader.java
+++ java/src/proxy/Downloader.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.net.URLConnection;
 import java.util.Date;
 
 import util.ByteArrayWrapper;
@@ -11,6 +12,43 @@ import util.ByteArrayWrapper;
 
 public class Downloader {
 
+	public static boolean isModifiedSince(String url, Date d) {
+		boolean result = false;
+		URL u = null;
+		try {
+			u = new URL(url);
+		} catch (MalformedURLException ex) {
+			ex.printStackTrace();
+		}
+		URLConnection uc = null;
+		try {
+			uc = u.openConnection();
+		} catch (MalformedURLException ex) {
+			ex.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		uc.setIfModifiedSince(d.getTime());
+		InputStream in = null;
+		try {
+			in = uc.getInputStream();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		int b = 0;
+		//System.out.println(in);
+		try {
+			b = in.read();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		if (b == -1) {
+			return false;
+		} else {
+			return true;
+		}
+	}
+	
 	public static DownloadResult download(String url) {
 		DownloadResult result = new DownloadResult();
 		ByteArrayWrapper byteAW = new ByteArrayWrapper();
---
+++ java/src/proxy/cache/CacheManager.java
@@ -0,0 +1,31 @@
+package proxy.cache;
+
+import java.util.*;
+
+import proxy.*;
+import proxy.Downloader;
+
+public class CacheManager {
+
+	/**
+	 * <URL as a string, CacheData>
+	 */
+	private HashMap<String, CacheData> cache;
+	
+	public CacheManager() {
+		
+	}
+	
+	public byte[] getData(String url, Downloader downloader) {
+		CacheData o = cache.get(url);
+		if (o == null) {
+			DownloadResult dr = downloader.download(url);
+			CacheData c = new CacheData(dr.getDate(), dr.getData());
+			cache.put(url, c);
+			return dr.getData();
+		} else {
+			//check if-modified since
+		}
+		return o.getData();
+	}
+}
