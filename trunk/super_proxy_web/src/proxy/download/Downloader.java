922d020908b4ce27e509ffb7185f4ba607b3dd63
chilltassapola <chilltassapola@7427e34b-43ad-5727-de77-e22b264e6e80>
2010-11-24 22:26:47 +0000

    move download

--- trunk/super_proxy_web/src/proxy/download/Downloader.java
+++ trunk/super_proxy_web/src/proxy/download/Downloader.java
@@ -56,49 +56,6 @@ public class Downloader {
 	 * @return
 	 */
 	public static DownloadResult download(String url) {
-		DownloadResult result = new DownloadResult();
-		ByteArrayWrapper byteAW = new ByteArrayWrapper();
-		URL netUrl = null;
-		try {
-			netUrl = new URL(url);
-		} catch (MalformedURLException e) {
-			e.printStackTrace();
-		}
-		InputStream in = null;
-		URLConnection urlConn = null;
-		
-		try {
-			urlConn = netUrl.openConnection();
-			in = urlConn.getInputStream();
-		} catch (IOException e) {
-			//e.printStackTrace();
-			return null;
-		}
-		int b = 0;
-		
-		do {
-			try {
-				b = in.read();
-			} catch (IOException e) {
-				//e.printStackTrace();
-				return null;
-			}
-			if (b != -1) {
-				byteAW.addByte((byte) b);
-			}
-		}
-		while (b != -1);
-		try {
-			in.close();
-		} catch (IOException e) {
-			//e.printStackTrace();
-			return null;
-		}
-		result.setData(byteAW.getFinalByteArray());
-		result.setDate(new Date());
-		result.setContentEncoding(urlConn.getContentEncoding());
-		result.setContentLength(urlConn.getContentLength());
-		result.setContentType(urlConn.getContentType());
-		return result;
+		return SingleThreadDownloader.download(url);
 	}
 }
