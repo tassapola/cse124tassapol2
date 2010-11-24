2daf0c8c275d4cf056b50ef1872b5c1b2f190bdd
chilltassapola <chilltassapola@7427e34b-43ad-5727-de77-e22b264e6e80>
2010-11-24 06:43:30 +0000

    get content type len

--- java/src/proxy/cache/CacheData.java
+++ java/src/proxy/cache/CacheData.java
@@ -2,6 +2,8 @@ package proxy.cache;
 
 import java.util.Date;
 
+import proxy.DownloadResult;
+
 public class CacheData {
 
 	/**
@@ -9,12 +11,42 @@ public class CacheData {
 	 */
 	private Date date;
 	private String filepath;
+	private String contentEncoding;
+	private int contentLength;
+	private String contentType;
 	
-	public CacheData(Date date, String filename) {
-		this.date = date;
-		this.filepath = filename;
+	public CacheData(DownloadResult dr, String filepath) {
+		this.date = dr.getDate();
+		this.filepath = filepath;
+		this.contentEncoding = dr.getContentEncoding();
+		this.contentLength = dr.getContentLength();
+		this.contentType = dr.getContentType();
 	}
 	
+	public String getContentEncoding() {
+		return contentEncoding;
+	}
+
+	public void setContentEncoding(String contentEncoding) {
+		this.contentEncoding = contentEncoding;
+	}
+
+	public int getContentLength() {
+		return contentLength;
+	}
+
+	public void setContentLength(int contentLength) {
+		this.contentLength = contentLength;
+	}
+
+	public String getContentType() {
+		return contentType;
+	}
+
+	public void setContentType(String contentType) {
+		this.contentType = contentType;
+	}
+
 	public Date getDate() {
 		return date;
 	}
