c2c0ff33791429e71c467272cfa19fe26243973b
chilltassapola <chilltassapola@7427e34b-43ad-5727-de77-e22b264e6e80>
2010-11-24 06:45:07 +0000

    removed bin

--- java/src/proxy/cache/UrlConstants.java
+++
@@ -1,7 +0,0 @@
-package proxy.cache;
-
-public class UrlConstants {
-	public static final String DEFAULT_TARGET_WEB_HOST_REWRITE_URL =
-									"http://cse124demo:8080/CheckCache?url=";
-	public static final String HADOOP_RELATIVE_ROOT = "/superproxy";
-}
