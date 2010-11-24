aba3cd275db56db7801f7aed1a352015565c17b4
chilltassapola <chilltassapola@7427e34b-43ad-5727-de77-e22b264e6e80>
2010-11-24 05:37:03 +0000

    url constants, test cache

--- java/src/proxy/cache/CacheManager.java
+++ java/src/proxy/cache/CacheManager.java
@@ -3,7 +3,9 @@ package proxy.cache;
 import java.util.*;
 
 import proxy.*;
+import proxy.hadoop.HdfsReader;
 import proxy.hadoop.HdfsWriter;
+import util.MyUtil;
 
 public class CacheManager {
 
@@ -13,22 +15,28 @@ public class CacheManager {
 	private HashMap<String, CacheData> cache;
 	
 	public CacheManager() {
-		
+		cache = new HashMap<String, CacheData>();
 	}
 		
-	public synchronized byte[] getData(String url, Downloader downloader) {
+	public synchronized byte[] getData(String url) {
 		CacheData o = cache.get(url);
 		if (o == null) {
+			System.out.println("case 1: new url");
 			DownloadResult dr = Downloader.download(url);
+			//rewrite url
+			dr.setData(MyUtil.getDataAfterModifyUrl(dr.getData(), url));
 			String newFileName = CacheUtil.getRelativeHadoopPath(url);
 			CacheData c = new CacheData(dr.getDate(), newFileName);
 			cache.put(url, c);
 			HdfsWriter writer = new HdfsWriter(dr.getData(), newFileName);
-			writer.start();
+			//writer.start();
+			//this version will block until write finishes
+			writer.write();
 			return dr.getData();
 		} else {
 			//check if-modified since
 			if (Downloader.isModifiedSince(url, o.getDate())) {
+				System.out.println("case 2: refresh cache");
 				//need to refresh cache
 				DownloadResult dr = Downloader.download(url);
 				String filepath = o.getFilepath();
@@ -38,9 +46,10 @@ public class CacheManager {
 				writer.start();
 				return dr.getData();
 			} else {
+				System.out.println("case 3: read data from hadoop fs");
 				//read data from hadoop fs
-				//TODO
-				return null;
+				HdfsReader reader = new HdfsReader(o.getFilepath());
+				return reader.read();
 			}
 		}
 	}
